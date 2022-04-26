f040aa7c5b000f5cfc35b86e54bf513bc6e81a1e
Ankush Banerjee <ankb223@gmail.com>
2022-04-26 17:17:06 +0530

    small model error fix

--- model/verified_users.js
+++ model/verified_users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
 
     username: {type:String , required: true , unique:true },
-    password: {type:String , required:true , inque: true}
+    password: {type:String , required:true , unique: true}
     },
     
     {collection:'verified_users'}
